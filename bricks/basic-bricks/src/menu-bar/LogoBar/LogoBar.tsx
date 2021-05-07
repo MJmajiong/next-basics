89794a52d714778e4e9070686c70ef09fb169118
astrid <astridli@easyop.cn>
2021-05-07 19:07:38 +0800

    feat(basic-bricks): Support to replace favicon.png
    refs BRICK_STORE-2092

--- bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
+++ bricks/basic-bricks/src/menu-bar/LogoBar/LogoBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "@next-libs/basic-components";
 import styles from "./LogoBar.module.css";
 import { ReactComponent as Logo } from "../../images/logo-3.1.svg";
@@ -6,6 +6,12 @@ import { getRuntime } from "@next-core/brick-kit";
 
 export function LogoBar(): React.ReactElement {
   const brand = getRuntime().getBrandSettings();
+  useEffect(() => {
+    if (brand.favicon_url) {
+      const linkDom = document.querySelector('link[rel="shortcut icon"]');
+      linkDom.href = brand.favicon_url;
+    }
+  }, [brand.favicon_url]);
   return (
     <div className={styles.logoBar}>
       <Link to="/" className={styles.logoLink}>
